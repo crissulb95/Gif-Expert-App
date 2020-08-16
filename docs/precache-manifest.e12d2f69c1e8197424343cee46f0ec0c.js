660453fcf324880b252897898cc52a9d574cd3e3
Cristian <cristian.sulbaran.r@gmail.com>
2020-08-16 16:01:20 -0300

    build doc creado y agregado para produccion

---
+++ docs/precache-manifest.e12d2f69c1e8197424343cee46f0ec0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dbb5dfcabcf2f892c30c40d0aad8b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f645a56a4c54ffa94d",
+    "url": "/static/css/main.0cf2a42d.chunk.css"
+  },
+  {
+    "revision": "76da65e5c0c7af143bb0",
+    "url": "/static/js/2.e8c6f352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8c6f352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f645a56a4c54ffa94d",
+    "url": "/static/js/main.a82dbf95.chunk.js"
+  },
+  {
+    "revision": "8aa548225c1bf62340ca",
+    "url": "/static/js/runtime-main.7c2dabf1.js"
+  }
+]);
